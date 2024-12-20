c5040be870834f9956e61f2eb1521e0f2a7ed481
dogisgreat <github@bigly.dog>
2024-12-20 12:26:06 -0500

    deterministic builds

--- build.rs
+++ build.rs
@@ -3,32 +3,10 @@
 #![deny(clippy::all, clippy::cargo, clippy::nursery, clippy::pedantic)]
 #![allow(clippy::cargo_common_metadata, clippy::wildcard_dependencies)]
 
-use std::{
-  error::Error,
-  process::{Command, Stdio},
-};
+fn main() {
+  println!("cargo:rustc-env=SAD_ARGV_UUID=4f3828bf-ebf6-4f46-b07b-7eb9e4ae4380");
 
-fn uuid() -> Result<String, Box<dyn Error>> {
-  #[cfg(target_family = "windows")]
-  let py = "python.exe";
-  #[cfg(target_family = "unix")]
-  let py = "python3";
-  let proc = Command::new(py)
-    .arg("-c")
-    .arg("import uuid; print(uuid.uuid4())")
-    .stdout(Stdio::piped())
-    .output()?;
-  assert!(proc.status.success());
-  let uuid = String::from_utf8(proc.stdout)?.trim().into();
-  Ok(uuid)
-}
+  println!("cargo:rustc-env=SAD_PREVIEW_UUID=b477f4c9-9fe7-4224-92cd-1632521ec2f0",);
 
-fn main() -> Result<(), Box<dyn Error>> {
-  println!("cargo:rustc-env=SAD_ARGV_UUID={uuid}", uuid = uuid()?);
-
-  println!("cargo:rustc-env=SAD_PREVIEW_UUID={uuid}", uuid = uuid()?);
-
-  println!("cargo:rustc-env=SAD_PATCH_UUID={uuid}", uuid = uuid()?);
-
-  Ok(())
+  println!("cargo:rustc-env=SAD_PATCH_UUID=cadfe8eb-0dae-46be-bb4a-a058330e62a4",);
 }
